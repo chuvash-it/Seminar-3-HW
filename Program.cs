ffde5493346518a24c318903674288bf20c78770
Sanya <a.kondratev9@gmail.com>
2022-11-21 22:17:24 +0300

    task21

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ Console.Write("z: ");
 int z2 = Convert.ToInt32(Console.ReadLine());
 double Distance(int x1, int y1, int z1, int x2, int y2, int z2)
 {
-  double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(x2 - x1, 2) + Math.Pow(x2 - x1, 2));
+  double result = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
   return result;
 }
 double distance = Distance(x1, y1, z1, x2, y2, z2);
